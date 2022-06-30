798fa89839316add963ed33f181d66eea45c7f99
Emil Kampp <100700894+av-emil@users.noreply.github.com>
2022-06-30 12:37:41 +0200

    Remove accessibility man
    This removes the accessibility man.

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -14,16 +14,6 @@ const AppHeader: React.FC<AppHeaderProps> = () => {
   return (
     <header className="flex w-full  items-center justify-between bg-brand-background dark:bg-brand-darkBackground p-[15px] md:p-[50px] dark:text-white">
       <nav className="flex space-x-[20px] md:space-x-[40px] items-center ">
-        <AccessibilityPopup isOpen={modal} onClose={() => setModal(false)} />
-        <a href="#">
-          <TbAccessible
-            onClick={() => setModal(true)}
-            aria-label="accessibility menu icon"
-            size="30px"
-            className="cursor-pointer"
-          />
-        </a>
-
         <Link to="/about">About</Link>
         <Link to="/faq">FAQ</Link>
         {/* <TextResizer /> */}
