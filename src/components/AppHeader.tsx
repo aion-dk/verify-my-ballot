454ee5cab28d9b7119490d50d7943d623ec3da18
Mads Christiansen <madc@aion.dk>
2022-06-30 15:50:58 +0200

    re-add link

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -13,17 +13,10 @@ const AppHeader: React.FC<AppHeaderProps> = () => {
 
   return (
     <header className="flex w-full  items-center justify-between bg-brand-background dark:bg-brand-darkBackground p-[15px] md:p-[50px] dark:text-white">
+      <Link to="/">Logo</Link>
       <nav className="flex space-x-[20px] md:space-x-[40px] items-center ">
-        {/* <AccessibilityPopup isOpen={modal} onClose={() => setModal(false)} />
-        <TbAccessible
-          onClick={() => setModal(true)}
-          aria-label="accessibility menu icon"
-          size="30px"
-          className="cursor-pointer"
-        /> */}
         <Link to="/about">About</Link>
         <Link to="/faq">FAQ</Link>
-        {/* <TextResizer /> */}
       </nav>
     </header>
   )
