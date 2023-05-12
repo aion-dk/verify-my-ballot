33dea45c67808515c357d9a0e507b78be4f207c6
Lukas Alex <lukas.alex@assemblyvoting.com>
2023-05-12 13:18:05 +0200

    Removing STRICT mode altogether

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { STRICT_MODE } from './constants'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-if (STRICT_MODE) {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-} else {
-  root.render(<App />)
-}
+root.render(<App />)
